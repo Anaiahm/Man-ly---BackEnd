865159a677447cc9a8baf9c0447ea5eae8a4b458
Anaiah <amaddox.dev@gmail.com>
2026-01-29 12:39:38 -0800

    added base url to all routes

---
+++ src/main/java/com/example/demo/controllers/TestController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello, %s!", name);
+	}
+
+    @GetMapping("/")
+    public String landing() {
+        return "Welcome to the demo application!";
+    }
+
+    @GetMapping("/bye")
+    public String bye() {
+        return "Goodbye from the demo application!";
+    }
+
+    @GetMapping("/signin")
+    public String signin() {
+        return "Sign in!";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "Sign up!";
+    }
+}
