757c65e9b179fc26fb1ab60f50a01bfdff4f426f
Anaiah <amaddox.dev@gmail.com>
2026-02-02 20:12:53 -0800

    Added Provider Service Tests

--- src/main/java/com/example/demo/services/ProviderServiceTest.java
+++ src/main/java/com/example/demo/services/ProviderServiceTest.java
@@ -1,5 +1,29 @@
 package com.example.demo.services;
 
-public class ProviderServiceTest {
-    
+import com.example.demo.models.ProviderCategory;
+import com.example.demo.repositories.ProviderRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProviderServiceTest {
+
+    private final ProviderService providerService =
+            new ProviderService(new ProviderRepository());
+
+    @Test
+    void shouldReturnAllProviders() {
+        var providers = providerService.getAllProviders();
+        assertThat(providers).isNotEmpty();
+    }
+
+    @Test
+    void shouldFilterByCategory() {
+        var providers = providerService.getProvidersByCategory(
+                ProviderCategory.MENTAL_HEALTH
+        );
+
+        assertThat(providers)
+                .allMatch(p -> p.getCategory() == ProviderCategory.MENTAL_HEALTH);
+    }
 }
