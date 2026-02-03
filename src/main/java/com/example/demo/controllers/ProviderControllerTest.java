e237f47a9c85cbd08507411a82aa13a2747f43af
Anaiah <amaddox.dev@gmail.com>
2026-02-02 20:11:50 -0800

    Added Provider Controller Tests

--- src/main/java/com/example/demo/controllers/ProviderControllerTest.java
+++ src/main/java/com/example/demo/controllers/ProviderControllerTest.java
@@ -1,5 +1,33 @@
 package com.example.demo.controllers;
 
-public class ProviderControllerTest {
-    
+import com.example.demo.services.ProviderService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProviderController.class)
+class ProviderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProviderService providerService;
+
+    @Test
+    void shouldReturnAllProviders() throws Exception {
+        mockMvc.perform(get("/api/providers"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturnProvidersByCategory() throws Exception {
+        mockMvc.perform(get("/api/providers/category/MENTAL_HEALTH"))
+                .andExpect(status().isOk());
+    }
 }
